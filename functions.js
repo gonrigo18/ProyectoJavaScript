371333399cf3f2224d9ffe8e30fc7b609c2b125c
gonrigo <gonzalo.rigobello18@gmail.com>
2022-07-15 16:30:41 -0300

    Version 4.7

--- functions.js
+++ functions.js
@@ -14,15 +14,20 @@ function showContent(id,e) {
 }
 
 function deposito() {
-    document.getElementById("error").style.display='none';
+    document.getElementById("error1").style.display='none';
 
     let valor= parseInt(document.getElementById("depositbtn").value);
     if((!isNaN(valor)) && (" " !== valor))
     {
         totalAccount = totalAccount + valor;
         document.getElementById("total").innerHTML=" $ " + totalAccount;
+        Swal.fire({
+            title:'Deposito satifactoria!',
+            icon:"success",
+            confirmButtonText:'Aceptar',
+        })
     }
-    else{
+    else {
         document.getElementById("error1").innerHTML="El campo no puede estar vacio";
         document.getElementById("error1").style.display='block'
     }
@@ -38,6 +43,11 @@ function retiro() {
         {
             totalAccount=totalAccount-valor;
             document.getElementById("total").innerHTML= " $  " + totalAccount;
+            Swal.fire({
+                title:'Retiro satifactoria!',
+                icon:"success",
+                confirmButtonText:'Aceptar',
+            })
         }
         else{
             document.getElementById("error").innerHTML="La cantidad no puede superar el saldo";
@@ -50,92 +60,128 @@ function retiro() {
 }
 
     function buyBtc(){
-        document.getElementById("error").style.display='none';
+        document.getElementById("errorbtc").style.display='none';
         let valor= parseInt(document.getElementById("buybtc").value);
         if(!isNaN(valor)){
             saldoBtc = saldoBtc + (valor / btc);
-            document.getElementById("btctotal").innerHTML= "  " + saldoBtc;
+            document.getElementById("btctotal").innerHTML= "  " + saldoBtc + " ($" + (saldoBtc*btc) + ")";
             Swal.fire({
                 title:'Compra satifactoria!',
                 icon:"success",
                 confirmButtonText:'Aceptar',
             })
+        }else {
+            document.getElementById("errorbtc").innerHTML="El campo no puede estar vacio y no pueden ser letras";
+            document.getElementById("errorbtc").style.display='block';
         }
     }
 
 function buyEth(){
-    document.getElementById("error").style.display='none';
+    document.getElementById("erroreth").style.display='none';
 
     let valor2= parseInt(document.getElementById("buyeth").value);
     if(!isNaN(valor2)){
         saldoEth = saldoEth + (valor2 / eth);
-        document.getElementById("ethtotal").innerHTML="  " + saldoEth;
+        document.getElementById("ethtotal").innerHTML="  " + saldoEth + " ($" + (saldoEth*eth) + ")";
         Swal.fire({
             title:'Compra satifactoria!',
             icon:"success",
             confirmButtonText:'Aceptar',
         })
+    }
+    else {
+        document.getElementById("erroreth").innerHTML="El campo no puede estar vacio y no pueden ser letras";
+        document.getElementById("erroreth").style.display='block';
     }
 }
 
 function buyUsdt(){
-    document.getElementById("error").style.display='none';
+    document.getElementById("errorusdt").style.display='none';
 
     let valor3= parseInt(document.getElementById("buyusdt").value);
     if(!isNaN(valor3)){
         saldoUsdt = saldoUsdt + (valor3 / usdt);
-        document.getElementById("usdttotal").innerHTML="  " + saldoUsdt;
+        document.getElementById("usdttotal").innerHTML="  " + saldoUsdt  + " ($" + (saldoUsdt*usdt) + ")";
         Swal.fire({
             title:'Compra satifactoria!',
             icon:"success",
             confirmButtonText:'Aceptar',
         })
     }
+    else {
+        document.getElementById("errorusdt").innerHTML="El campo no puede estar vacio y no pueden ser letras";
+        document.getElementById("errorusdt").style.display='block';
+    }
 }
 
 function investBtc(){
-    document.getElementById("errorbtc").style.display='none';
+    document.getElementById("errorbtc1").style.display='none';
 
     let valor= parseFloat(document.getElementById("investbtc").value);
     if(!isNaN(valor)){
         if ((valor+btcInvested) > saldoBtc){
-            document.getElementById("errorbtc").innerHTML="La cantidad no puede superar el saldo";
-            document.getElementById("errorbtc").style.display='block';
+            document.getElementById("errorbtc1").innerHTML="La cantidad no puede superar el saldo";
+            document.getElementById("errorbtc1").style.display='block';
         }
         else{
         btcInvested = btcInvested + valor;
-        document.getElementById("btcinvesttotal").innerHTML= "  " + btcInvested;
+        document.getElementById("btcinvesttotal").innerHTML= "  " + btcInvested + " ($" + (btcInvested*btc) + ")";
+        Swal.fire({
+                title:'Inversión realizada!',
+                icon:"success",
+                confirmButtonText:'Aceptar',
+            })
         }
+    }else {
+        document.getElementById("errorbtc1").innerHTML="El campo no puede estar vacio y no pueden ser letras";
+        document.getElementById("errorbtc1").style.display='block';
     }
 }
 function investEth(){
-    document.getElementById("erroreth").style.display='none';
+    document.getElementById("erroreth1").style.display='none';
 
     let valor= parseFloat(document.getElementById("investeth").value);
     if(!isNaN(valor)){
         if ((valor+ethInvested) > saldoEth){
-            document.getElementById("erroreth").innerHTML="La cantidad no puede superar el saldo";
-            document.getElementById("erroreth").style.display='block';
+            document.getElementById("erroreth1").innerHTML="La cantidad no puede superar el saldo";
+            document.getElementById("erroreth1").style.display='block';
         }
         else{
         ethInvested = ethInvested + valor;
-        document.getElementById("ethinvesttotal").innerHTML= "  " + ethInvested;
+        document.getElementById("ethinvesttotal").innerHTML= "  " + ethInvested + " ($" + (ethInvested*eth) + ")";
+        Swal.fire({
+            title:'Inversión realizada!',
+            icon:"success",
+            confirmButtonText:'Aceptar',
+        })
         }
+    }else {
+        document.getElementById("erroreth1").innerHTML="El campo no puede estar vacio y no pueden ser letras";
+        document.getElementById("erroreth1").style.display='block';
     }
 }
 function investUsdt(){
-    document.getElementById("errorusdt").style.display='none';
+    document.getElementById("errorusdt1").style.display='none';
 
     let valor= parseFloat(document.getElementById("investusdt").value);
     if(!isNaN(valor)){
         if ((valor+usdtInvested) > saldoUsdt){
-            document.getElementById("errorusdt").innerHTML="La cantidad no puede superar el saldo";
-            document.getElementById("errorusdt").style.display='block';
+            document.getElementById("errorusdt1").innerHTML="La cantidad no puede superar el saldo";
+            document.getElementById("errorusdt1").style.display='block';
         }
         else{
         usdtInvested = usdtInvested + valor;
-        document.getElementById("usdtinvesttotal").innerHTML= "  " + usdtInvested;
+        document.getElementById("usdtinvesttotal").innerHTML= "  " + usdtInvested + " ($" + (usdtInvested*usdt) + ")";
+        Swal.fire({
+            title:'Inversión realizada!',
+            icon:"success",
+            confirmButtonText:'Aceptar',
+        })
         }
+    }
+    else {
+        document.getElementById("errorusdt1").innerHTML="El campo no puede estar vacio y no pueden ser letras";
+        document.getElementById("errorusdt1").style.display='block';
     }
 }
 
